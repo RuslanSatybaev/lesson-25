d838f2e233cc638c17c0133e3083418b91a2dad3
Ruslan Satybaev <medtehnik850@gmail.com>
2024-04-21 14:36:47 +0700

    Lesson-2: Interface Runnable

--- src/com/dmdev/ThreadDemo.java
+++ src/com/dmdev/ThreadDemo.java
@@ -2,6 +2,18 @@ package com.dmdev;
 
 public class ThreadDemo {
     public static void main(String[] args) {
+        var simpleRunnable = new SimpleRunnable();
+        var runnableThread = new Thread(simpleRunnable);
+        var thread = new Thread(() -> System.out.println("Hello from lambda: " + Thread.currentThread().getName()));
+
+        runnableThread.start();
+        thread.start();
+        try {
+            runnableThread.join();
+            thread.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
         System.out.println(Thread.currentThread().getName());
     }
 }

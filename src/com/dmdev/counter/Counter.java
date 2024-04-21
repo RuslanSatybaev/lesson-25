2c3230e482c2bcf7bf37fe0a6155bc6cbc5cd150
Ruslan Satybaev <medtehnik850@gmail.com>
2024-04-21 15:38:53 +0700

    Lesson-5: Synchronized

--- src/com/dmdev/counter/Counter.java
+++ src/com/dmdev/counter/Counter.java
@@ -2,13 +2,24 @@ package com.dmdev.counter;
 
 public class Counter {
 
+    private static String description;
     private int count;
 
-    public void increment() {
-        count++;
+    public static void init() {
+        synchronized (Counter.class) {
+            if (description == null) {
+                description = "Test description";
+            }
+        }
     }
 
-    public void decrement() {
+    public void increment() {
+        synchronized (this) {
+            count++;
+        }
+    }
+
+    public synchronized void decrement() {
         count--;
     }
 
